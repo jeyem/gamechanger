1df21a98e62cf2a45cacf8c8423558d3d0621d26
Ehsan Mahmoudi <e.mahmoudi98@gmail.com>
2017-07-09 22:39:37 +0430

    game owners

--- src/game/controller.go
+++ src/game/controller.go
@@ -1,9 +1,9 @@
 package game
 
 import (
+	"my/gamechanger/src/user"
 	"strconv"
 
-	"github.com/flosch/pongo2"
 	"github.com/labstack/echo"
 )
 
@@ -14,7 +14,44 @@ func list(e echo.Context) error {
 		page = 1
 	}
 	games := LoadGames(10, page, params)
-	return e.Render(200, "games.html", pongo2.Context{
+	return e.Render(200, "games.html", echo.Map{
 		"games": games,
+	})
+}
+
+func viewGame(e echo.Context) error {
+	gameID := e.Param("game_id")
+	if !IsObjectIdHex(gameID) {
+		return e.Render(403, "403.html", echo.Map{})
+	}
+	game := new(Model)
+	if err := game.Load(gameID); err != nil {
+		return e.Render(404, "404.html", echo.Map{})
+	}
+	return e.Render(200, "game-view.html", echo.Map{
+		"game": game,
+	})
+}
+
+func haveGame(e echo.Context) error {
+	gameID := e.Param("game_id")
+	if !IsObjectIdHex(gameID) {
+		return e.Render(403, "403.html", echo.Map{})
+	}
+	game := new(Model)
+	if err := game.Load(gameID); err != nil {
+		return e.Render(404, "404.html", echo.Map{})
+	}
+	c := e.(*user.Context)
+	geme.AppendOwner(c.User.ID)
+	if err := game.Save(); err != nil {
+		return e.Render(403, "game-view.html", echo.Map{
+			"error": err.Error(),
+			"game":  game,
+		})
+	}
+	return e.Render(200, "game-view.html", echo.Map{
+		"game": game,
+		"msg":  "success append",
 	})
 }
--- src/game/model.go
+++ src/game/model.go
@@ -9,20 +9,25 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
-type Game struct {
-	ID          bson.ObjectId   `bson:"_id,omitempty"`
-	Type        string          `bson:"type"`
-	Name        string          `bson:"name"`
-	Description string          `bson: "description"`
-	ReleaseDate time.Time       `bson: "release_date"`
-	Images      []string        `bson:"images"`
-	SourceURL   string          `bson:"source_url"`
-	Owners      []bson.ObjectId `bson:"owners"`
-	Tags        []string        `bson:"tags"`
-	Keywords    []string        `bson:"keywords"`
+type Owner struct {
+	UserID    bson.ObjectId `bson:"user"`
+	OwnedDate time.Time     `bson:"owned_date"`
 }
 
-func (Game) Meta() []mgo.Index {
+type Game struct {
+	ID          bson.ObjectId `bson:"_id,omitempty"`
+	Type        string        `bson:"type"`
+	Name        string        `bson:"name"`
+	Description string        `bson: "description"`
+	ReleaseDate time.Time     `bson: "release_date"`
+	Images      []string      `bson:"images"`
+	SourceURL   string        `bson:"source_url"`
+	Owners      []Owner       `bson:"owners"`
+	Tags        []string      `bson:"tags"`
+	Keywords    []string      `bson:"keywords"`
+}
+
+func (Model) Meta() []mgo.Index {
 	return []mgo.Index{
 		{
 			Key: []string{"keywords"},
@@ -30,13 +35,13 @@ func (Game) Meta() []mgo.Index {
 	}
 }
 
-func (g *Game) setKeywords() {
+func (g *Model) setKeywords() {
 	g.Keywords = g.Tags
 	year := strconv.Itoa(g.ReleaseDate.Year())
 	g.Keywords = append(g.Keywords, g.Name, g.Type, year)
 }
 
-func (g *Game) Save() error {
+func (g *Model) Save() error {
 	if g.ID.Valid() {
 		return g.Update()
 	}
@@ -45,18 +50,28 @@ func (g *Game) Save() error {
 	return application.DB.Create(g)
 }
 
-func (g *Game) Update() error {
+func (g *Model) Update() error {
 	return db.Update(g)
 }
 
-func LoadGames(limit, page int, filter ...string) (res []Game) {
+func (g *Model) AppendOwner(id bson.ObjectId) {
+	for _, owner := range g.Owners {
+		if owner.UserID == id {
+			return
+		}
+	}
+	g.Owners = append(g.Owners, Owner{id, time.Now()})
+	g.Save()
+}
+
+func LoadModels(limit, page int, filter ...string) (res []Model) {
 	db.Where(bson.M{"keywors": bson.M{
 		"$in": filter,
 	}}).Find(&res)
 	return res
 }
 
-func TotalGamePages(limit int) int {
-	count := db.Collection(&Game{}).Count()
+func TotalModelPages(limit int) int {
+	count := db.Collection(&Model{}).Count()
 	return int(float32(count)/float32(limit)) + 1
 }
